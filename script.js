3d76a5cd5302aefcb274decbd3b9af984c009b65
unknown <wirawanaryo99@gmail.com>
2026-03-10 18:46:08 +0700

    just a little more

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const addButton = document.getElementById('addButton');
-const inputNewBookDialog = document.getElementById('inputNewBook')
-
-addButton.onclick =() => inputNewBookDialog.showModal();
-
-
-
 const myLibrary = [];
 
 function Book(author, title, pages ) {
@@ -12,12 +5,31 @@ function Book(author, title, pages ) {
   this.author = author;
   this.title = title;
   this.pages = pages;
-
-
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(authorInput,titleInput,pageInput) {
   // take params, create a book then store it in the array
+  const newBook = new Book(authorInput,titleInput,pageInput);
+  myLibrary.push(Book);
 }
 
+const addButton = document.getElementById('addButton');
+const inputNewBookDialog = document.getElementById('inputNewBook')
+addButton.onclick =() => inputNewBookDialog.showModal();
 
+const submitInputButton = document.getElementById('submitInputButton')
+submitInputButton.onclick = () => {
+  const bookForm = document.getElementById("bookForm")
+  const authorInput = document.getElementById('authorInput').value;
+  const titleInput = document.getElementById('titleInput').value;
+  const pageInput = document.getElementById('pageInput').value;
+
+  if (authorInput && titleInput && pageInput){        
+    addBookToLibrary(authorInput,titleInput,pageInput);
+    inputNewBookDialog.close(); 
+    bookForm.reset();         
+    console.log(myLibrary); 
+  } else{
+    alert("Please fill all data!");
+  }
+}
